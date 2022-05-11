2d2b2cdf052d76183425f21126bd39a4622d7e5f
motylas <hgruda68@gmail.com>
2022-05-11 05:02:21 +0200

    elias decoding changes

--- Lista3/src/LZW.java
+++ Lista3/src/LZW.java
@@ -1,8 +1,18 @@
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 
 public class LZW {
+    final static int dictUTF = 1112064;
+    final static int dictASCII = 256;
     public static List<Integer> lzwE(String text) {
-        int dictSize = 256;
+        int dictSize  = dictASCII;
         Map<String, Integer> dictionary = new HashMap<>();
         for (int i = 0; i < dictSize; i++) {
             dictionary.put(String.valueOf((char) i), i);
@@ -27,7 +37,7 @@ public class LZW {
     }
 
     public static String lzwD(List<Integer> encodedText) {
-        int dictSize = 256;
+        int dictSize = dictASCII;
         Map<Integer, String> dictionary = new HashMap<>();
         for (int i = 0; i < dictSize; i++) {
             dictionary.put(i, String.valueOf((char) i));
@@ -70,18 +80,18 @@ public class LZW {
         return encodedNumbers;
     }
 
-    public static List<Integer> eliasOD(List<String> numbersToDecode) {
+    public static List<Integer> eliasOD(String input) {
         List<Integer> decodedNumbers = new ArrayList<>();
-        while (!numbersToDecode.isEmpty()) {
-            String number = numbersToDecode.remove(0);
+        while (!input.isEmpty()) {
             int n = 1;
-            while (number.charAt(0) == '1') {
+            while (input.charAt(0) == '1') {
                 int len = n;
                 n = 0;
-                String temp = number.substring(0, len + 1);
+                String temp = input.substring(0, len + 1);
                 n = Integer.parseInt(temp, 2);
-                number = number.substring(len + 1);
+                input = input.substring(len + 1);
             }
+            input = input.substring(1);
             decodedNumbers.add(n);
         }
         return decodedNumbers;
@@ -100,23 +110,40 @@ public class LZW {
         return encodedNumbers;
     }
 
-    public static List<Integer> eliasGD(List<String> numbersToDecode){
+    public static List<Integer> eliasGD(String input) {
         List<Integer> decodedNumbers = new ArrayList<>();
-        while (!numbersToDecode.isEmpty()) {
-            String number = numbersToDecode.remove(0);
+        while (!input.isEmpty()) {
             int n = 1;
-            while(number.charAt(0) != '1'){
-                number = number.substring(1);
+            while (input.charAt(0) != '1') {
+                input = input.substring(1);
                 n++;
             }
-            String temp = number.substring(0,n);
+            String temp = input.substring(0, n);
             int output = Integer.parseInt(temp, 2);
             decodedNumbers.add(output);
+            input = input.substring(n);
         }
         return decodedNumbers;
     }
 
-    public static List<String> eliasDE(List<Integer> numbersToEncode){
+//    public static List<Integer> eliasGD(List<String> numbersToDecode) {
+//        List<Integer> decodedNumbers = new ArrayList<>();
+//        while (!numbersToDecode.isEmpty()) {
+//            String number = numbersToDecode.remove(0);
+//            int n = 1;
+//            while (number.charAt(0) != '1') {
+//                number = number.substring(1);
+//                n++;
+//            }
+//            String temp = number.substring(0, n);
+//            System.out.println(number.substring(n));
+//            int output = Integer.parseInt(temp, 2);
+//            decodedNumbers.add(output);
+//        }
+//        return decodedNumbers;
+//    }
+
+    public static List<String> eliasDE(List<Integer> numbersToEncode) {
         List<String> encodedNumbers = new ArrayList<>();
         while (!numbersToEncode.isEmpty()) {
             StringBuilder output = new StringBuilder();
@@ -129,14 +156,14 @@ public class LZW {
             lengthOfLen = (int) Math.floor(log2(len));
 
             output.append("0".repeat(Math.max(0, lengthOfLen)));
-            for (int i = lengthOfLen; i>=0; --i){
+            for (int i = lengthOfLen; i >= 0; --i) {
                 if (((len >> i) & 1) == 1) {
-                    output.append( "1");
+                    output.append("1");
                 } else output.append("0");
             }
-            for (int i = len-2; i>=0;i--){
+            for (int i = len - 2; i >= 0; i--) {
                 if (((number >> i) & 1) == 1) {
-                    output.append( "1");
+                    output.append("1");
                 } else output.append("0");
             }
             encodedNumbers.add(output.toString());
@@ -144,57 +171,57 @@ public class LZW {
         return encodedNumbers;
     }
 
-    public static List<Integer> eliasDD(List<String> numbersToDecode){
+    public static List<Integer> eliasDD(List<String> numbersToDecode) {
         List<Integer> decodedNumbers = new ArrayList<>();
-        while (!numbersToDecode.isEmpty()){
+        while (!numbersToDecode.isEmpty()) {
             String number = numbersToDecode.remove(0);
             int l = 1;
-            while(number.charAt(0) == '0'){
+            while (number.charAt(0) == '0') {
                 number = number.substring(1);
                 l++;
             }
-            String temp = number.substring(0,l);
+            String temp = number.substring(0, l);
             number = number.substring(l);
             int n = Integer.parseInt(temp, 2) - 1;
-            int output = Integer.parseInt(number.substring(0,n),2);
+            int output = Integer.parseInt(number.substring(0, n), 2);
             output += Math.pow(2, n);
             decodedNumbers.add(output);
         }
         return decodedNumbers;
     }
 
-    public static List<String> fibbE(List<Integer> numbersToEncode){
+    public static List<String> fibbE(List<Integer> numbersToEncode) {
         List<String> encodedNumbers = new ArrayList<>();
-        while(!numbersToEncode.isEmpty()){
+        while (!numbersToEncode.isEmpty()) {
             StringBuilder output = new StringBuilder();
             int number = numbersToEncode.remove(0);
             int n = 2;
             int fib = 1;
-            while (true){
+            while (true) {
                 int temp;
-                if ((temp = fibbNumber(n+1)) > number){
+                if ((temp = fibbNumber(n + 1)) > number) {
                     break;
                 }
                 n++;
                 fib = temp;
             }
             output.append("0".repeat(Math.max(0, n - 1)));
-            output.setCharAt(n-2,'1');
+            output.setCharAt(n - 2, '1');
             output.append('1');
             number -= fib;
-            while (true){
+            while (true) {
                 if (number == 0) break;
                 n = 2;
                 fib = 1;
-                while (true){
+                while (true) {
                     int temp;
-                    if ((temp = fibbNumber(n+1)) > number){
+                    if ((temp = fibbNumber(n + 1)) > number) {
                         break;
                     }
                     n++;
                     fib = temp;
                 }
-                output.setCharAt(n-2,'1');
+                output.setCharAt(n - 2, '1');
                 number -= fib;
             }
             encodedNumbers.add(output.toString());
@@ -202,16 +229,16 @@ public class LZW {
         return encodedNumbers;
     }
 
-    public static List<Integer> fibbD(List<String> numbersToDecode){
+    public static List<Integer> fibbD(List<String> numbersToDecode) {
         List<Integer> decodedNumbers = new ArrayList<>();
-        while(!numbersToDecode.isEmpty()){
+        while (!numbersToDecode.isEmpty()) {
             String number = numbersToDecode.remove(0);
-            number = number.substring(0,number.length()-1);
+            number = number.substring(0, number.length() - 1);
             int output = 0;
             int index = 0;
-            for(char character: number.toCharArray()){
-                if (character == '1'){
-                    output += fibbNumber(index+2);
+            for (char character : number.toCharArray()) {
+                if (character == '1') {
+                    output += fibbNumber(index + 2);
                 }
                 index++;
             }
@@ -219,35 +246,54 @@ public class LZW {
         }
         return decodedNumbers;
     }
+
     private static double log2(int number) {
         return Math.log(number) / Math.log(2);
     }
 
-    private static int fibbNumber(int n){
+    private static int fibbNumber(int n) {
         if (n == 0) return 0;
         if (n == 1) return 1;
         if (n == 2) return 1;
-        return fibbNumber(n-1) + fibbNumber(n-2);
+        return fibbNumber(n - 1) + fibbNumber(n - 2);
     }
 
-    public static void main(String[] args) {
-//        List<Integer> compressed = lzwE("abababab");
-//        System.out.println(compressed);
-//        var code = eliasDE(compressed);
-//        System.out.println(code);
-//        var decode = eliasDD(code);
-//        System.out.println(decode);
-//        String decompressed = lzwD(decode);
-//        System.out.println(decompressed);
-        List<Integer> test = new ArrayList<>();
-        test.add(5);
-        test.add(3);
-        test.add(11);
-        test.add(12);
-        test.add(1);
-        test.add(14);
-        var x = fibbE(test);
-        System.out.println(x);
-        System.out.println(fibbD(x));
+//    public static void main(String[] args) throws IOException {
+//        Path path = Paths.get("C:\\Users\\hgrud\\Documents\\GitHub\\SEM4-KKD\\testy1\\pan-tadeusz.txt");
+//        byte[] data = Files.readAllBytes(path);
+//        String test = new String(data, StandardCharsets.US_ASCII);
+//        List<Integer> compressed = lzwE(test);
+////        List<String> coded = eliasDE(compressed);
+////        for (String word: coded){
+////
+////        }
+//    }
+
+    public static void main(String[] args) throws IOException {
+        long start = System.currentTimeMillis();
+        String fileName = "C:\\Users\\hgrud\\Documents\\GitHub\\SEM4-KKD\\testy1\\pan-tadeusz.txt";
+        DataInputStream inputStream = new DataInputStream(new FileInputStream(fileName));
+        int byteRead = -1;
+        int codeLength = 0;
+        Integer count = null;
+        StringBuilder input = new StringBuilder();
+        while ((byteRead = inputStream.read()) != -1) {
+            codeLength++;
+            input.append((char) byteRead);
+        }
+        List<Integer> x = lzwE(input.toString());
+        var y = eliasGE(x);
+        System.out.println("Ys");
+        StringBuilder all = new StringBuilder();
+        for (String st: y){
+            all.append(st);
+//            System.out.println(st);
+        }
+        System.out.println("YS2");
+        var z = eliasGD(all.toString());
+        System.out.println("YS3");
+        System.out.println(lzwD(z));
+        long end = System.currentTimeMillis();
+        System.out.println((end-start));
     }
 }
